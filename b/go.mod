cc7ae3e877444773f1190d560e20ca1e9bf95a30
Dan Enman <daniel.enman@snyk.io>
2021-05-31 14:35:14 -0300

    b/1.0.0 deps

--- b/go.mod
+++ b/go.mod
@@ -1,3 +1,5 @@
 module github.com/enmand/synk-fixtures-go-mvs/b
 
 go 1.16
+
+require github.com/enmand/synk-fixtures-go-mvs/d v1.2.0 // indirect
--- b/main.go
+++ b/main.go
@@ -1,5 +1,11 @@
 package b
 
+import "github.com/enmand/synk-fixtures-go-mvs/d"
+
 func GetB() string {
 	return "b:1.0.0"
+}
+
+func GetD() string {
+	return d.GetD()
 }
