85ba055f17098c3686ecdc19a70933efaf5b0c2d
Dan Enman <daniel.enman@snyk.io>
2021-05-31 14:33:35 -0300

    a/1.0.0 deps

--- a/go.mod
+++ a/go.mod
@@ -1,3 +1,9 @@
 module github.com/enmand/synk-fixtures-go-mvs/a
 
 go 1.16
+
+require (
+	github.com/enmand/synk-fixtures-go-mvs/b v1.0.0 // indirect
+	github.com/enmand/synk-fixtures-go-mvs/c v1.0.0 // indirect
+	github.com/enmand/synk-fixtures-go-mvs/e v1.5.0 // indirect
+)
--- a/main.go
+++ a/main.go
@@ -1,5 +1,23 @@
 package a
 
+import (
+	"github.com/enmand/synk-fixtures-go-mvs/b"
+	"github.com/enmand/synk-fixtures-go-mvs/c"
+	"github.com/enmand/synk-fixtures-go-mvs/e"
+)
+
 func GetA() string {
 	return "a:1.0.0"
+}
+
+func GetB() string {
+	return b.GetB()
+}
+
+func GetC() string {
+	return c.GetC()
+}
+
+func GetE() string {
+	return e.GetE()
 }
